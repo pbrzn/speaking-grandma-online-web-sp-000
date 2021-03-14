55c4e05583b12d2b39d9d50c516e5aa59c3a15cf
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-14 00:18:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 def speak_to_grandma(phrase)
-  if phrase==phrase.upcase && phrase!="I LOVE YOU GRANDMA!"
+  if !!phrase.upcase && phrase!="I LOVE YOU GRANDMA!"
     "NO, NOT SINCE 1938!"
   elsif phrase=="I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
